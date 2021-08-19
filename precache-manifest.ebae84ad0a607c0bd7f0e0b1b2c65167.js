1e0f8c23fdb01ac7831c00328edcc11e2fd5155b
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-08-19 10:57:37 +0200

    first commit

---
+++ precache-manifest.ebae84ad0a607c0bd7f0e0b1b2c65167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78c0e721a9962ddff5c1e734628d1fe",
+    "url": "/chat-app-react/index.html"
+  },
+  {
+    "revision": "de4a5de3bfd5c429495a",
+    "url": "/chat-app-react/static/css/main.2fe3dd23.chunk.css"
+  },
+  {
+    "revision": "5d7a75085e5a5bfd7d8e",
+    "url": "/chat-app-react/static/js/2.4df6f280.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/chat-app-react/static/js/2.4df6f280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4a5de3bfd5c429495a",
+    "url": "/chat-app-react/static/js/main.361fc78b.chunk.js"
+  },
+  {
+    "revision": "36b7dcd12c787faf08b2",
+    "url": "/chat-app-react/static/js/runtime-main.1783be93.js"
+  }
+]);
